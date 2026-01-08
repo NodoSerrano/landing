cd2af974728cf71e9f20e96ca7f7a43186e7db67
Lucianosc <luchotandil90@gmail.com>
2026-01-08 15:39:15 +0100

    feat: add google maps clickable address on footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,24 @@
 "use client";
 
 import type React from "react";
+import LocationIcon from "./svgs/location-icon";
 
 export default function Footer() {
   return (
     <footer className="bg-slate-900 border-t border-violet-400/30 py-6 md:py-8">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="text-xl mb-6 md:mb-0">
-            Tandil, Argentina
+          <div className="text-xl mb-6 md:mb-0 flex items-center gap-2">
+            <a
+              href="https://www.google.com/maps/place/San%20Martin%20864%2C%20Tandil%2C%20Buenos%20Aires%2C%20Argentina"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-slate-400 hover:text-slate-300 transition-colors flex gap-2"
+              title="View on Google Maps"
+            >
+              <LocationIcon className="w-6 h-6" />
+              <span>Tandil, Buenos Aires, Argentina.</span>
+            </a>
           </div>
           <div className="text-sm">
             © {new Date().getFullYear()} Nodo Serrano. Todos los derechos
